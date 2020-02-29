f6441ab4422bab93397067b6acbc22be5e2c87e5
Christian Quest <github@cquest.org>
2020-02-29 10:13:24 +0100

    Mise à jour pour import 2018

--- rpg_download_import.sh
+++ rpg_download_import.sh
@@ -2,12 +2,12 @@
 
 # Script de téléchargement et chargement des données RPG dans postgresql
 
-for CMD in wget unzip ogr2ogr psql
+for CMD in wget unzip ogr2ogr psql 7z
 do
   if [ "$(which $CMD)" = "" ]
   then
     echo "$CMD non installé"
-    echo "sudo apt install wget unzip gdal-bin postgresql"
+    echo "sudo apt install wget unzip gdal-bin postgresql p7zip-full"
     exit
   fi
 done
@@ -15,17 +15,10 @@ done
 mkdir -p data
 cd data
 
-# 2017
-wget -N -c http://data.cquest.org/registre_parcellaire_graphique/2017/RPG_2-0_SHP_LAMB93_FR-2017-PARCELLES.zip
-unzip -jn RPG_2-0_SHP_LAMB93_FR-2017-PARCELLES.zip
-
-# import postgresql avec reprojection WGS84
-ogr2ogr -f postgresql PG:dbname=$USER -nln rpg_2017_parcelles -nlt geometry -t_srs EPSG:4326 PARCELLES_GRAPHIQUES.shp
-psql -c "
-alter table rpg_2017_parcelles drop ogc_fid;
-create or replace view rpg_parcelles as
-  select 2017 as annee, id_parcel as id_parcelle, code_cultu as code_culture, code_group as code_groupe, culture_d1, culture_d2, wkb_geometry rpg_2017_parcelles;
-"
+# 2015
+wget -N -c http://data.cquest.org/registre_parcellaire_graphique/2015/RPG_2-0_SHP_LAMB93_FR-2015-PARCELLES.zip
+unzip -jn RPG_2-0_SHP_LAMB93_FR-2015-PARCELLES.zip
+ogr2ogr -f postgresql PG:dbname=$USER -nln rpg_2015_parcelles -nlt geometry -t_srs EPSG:4326 PARCELLES_GRAPHIQUES.shp
 # nettoyage
 rm -f PARCELLES*
 
@@ -34,31 +27,32 @@ wget -N -c http://data.cquest.org/registre_parcellaire_graphique/rpg2016-wgs84-p
 unzip -jn rpg2016-wgs84-plus.zip
 ogr2ogr -f postgresql PG:dbname=$USER -nln rpg_2016_parcelles -nlt geometry rpg2016.shp
 rm -f rpg2016.*
-psql -c "
-alter table rpg_2016_parcelles drop ogc_fid;
-create or replace view rpg_parcelles as
-  select 2017 as annee, id_parcel as id_parcelle, code_cultu as code_culture, code_group as code_groupe, culture_d1, culture_d2, wkb_geometry rpg_2017_parcelles
-union
-  select 2016 as annee, id_parcel as id_parcelle, code_cultu as code_culture, code_group as code_groupe, culture_d1, culture_d2, wkb_geometry from rpg_2016_parcelles;
-"
 
+# 2017
+wget -N -c http://data.cquest.org/registre_parcellaire_graphique/2017/RPG_2-0_SHP_LAMB93_FR-2017-PARCELLES.zip
+unzip -jn RPG_2-0_SHP_LAMB93_FR-2017-PARCELLES.zip
+# import postgresql avec reprojection WGS84
+ogr2ogr -f postgresql PG:dbname=$USER -nln rpg_2017_parcelles -nlt geometry -t_srs EPSG:4326 PARCELLES_GRAPHIQUES.shp
 # nettoyage
-rm -f rpg2016.*
+rm -f PARCELLES*
 
-# 2015
-wget -N -c http://data.cquest.org/registre_parcellaire_graphique/2015/RPG_2-0_SHP_LAMB93_FR-2015-PARCELLES.zip
-unzip -jn RPG_2-0_SHP_LAMB93_FR-2015-PARCELLES.zip
-ogr2ogr -f postgresql PG:dbname=$USER -nln rpg_2015_parcelles -nlt geometry -t_srs EPSG:4326 PARCELLES_GRAPHIQUES.shp
 
+# 2018
+wget -N -nv http://data.cquest.org/registre_parcellaire_graphique/2018/RPG_2-0__SHP_LAMB93_FR-2018_2018-01-15.7z
+7z x RPG_2-0__SHP_LAMB93_FR-2018_2018-01-15.7z
+
+# import postgresql avec reprojection WGS84
+ogr2ogr -f pgdump -nln rpg_2018_parcelles -nlt geometry -t_srs EPSG:4326 RPG*2018*/*/*/*/PARCELLES_GRAPHIQUES.shp | psql $USER
 psql -c "
-alter table rpg_2015_parcelles drop ogc_fid;
+alter table rpg_2018_parcelles drop ogc_fid;
 create or replace view rpg_parcelles as
-  select 2017 as annee, id_parcel as id_parcelle, code_cultu as code_culture, code_group as code_groupe, culture_d1, culture_d2, wkb_geometry from rpg_2017_parcelles
+  SELECT 2018 as annee, id_parcel as id_parcelle, code_cultu as code_culture, code_group as code_groupe, culture_d1, culture_d2, wkb_geometry FROM rpg_2018_parcelles
+union
+  select 2017 as annee, id_parcel as id_parcelle, code_cultu as code_culture, code_group as code_groupe, culture_d1, culture_d2, wkb_geometry FROM rpg_2017_parcelles
 union
   select 2016 as annee, id_parcel as id_parcelle, code_cultu as code_culture, code_group as code_groupe, culture_d1, culture_d2, wkb_geometry from rpg_2016_parcelles
 union
   select 2015 as annee, id_parcel as id_parcelle, code_cultu as code_culture, code_group as code_groupe, culture_d1, culture_d2, wkb_geometry from rpg_2015_parcelles;
 "
-
 # nettoyage
-rm -f PARCELLES*
+rm -rf RPG*2018*
